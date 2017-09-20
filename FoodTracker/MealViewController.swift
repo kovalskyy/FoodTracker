57dd3761250c6bb5a45cecaab45101fe47e40b47
Kacper Kowalski <kowal@Mac-mini-Kacper.local>
2017-09-20 11:28:26 +0200

    implemented unwind segue for mealTableViewController

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+import os.log
 
 class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var mealTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    /*
+     This value is either passed by `MealTableViewController` in `prepare(for:sender:)`
+     or constructed as part of adding a new meal.
+     */
+    var meal: Meal?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +33,31 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         // Hide keyboard
         textField.resignFirstResponder()
-        
         return true
     }
+    
+    //MARK: Navigation
+    
+    // This method lets you configure a view controller before it's presented.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        super.prepare(for: segue, sender: sender)
+        
+        // Configure the destination view controller only when the save button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default,
+            type: .debug)
+            return
+        }
+        
+        let name = mealTextField.text ?? ""
+        let photo = imageView.image
+        let rating = ratingControl.rating
+        
+        // Set the meal to be passed to MealTableViewController after te unwind segue.
+        meal = Meal(name: name, photo: photo, rating: rating)
+    }
+    
     
     func textFieldDidEndEditing(_ textField: UITextField) {
     }
